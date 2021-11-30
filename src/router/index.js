cfc46169ce23f2bf39497cd3b3bc3c52cd0df015
jnx2280303225 <jiangnanxin@popicorns.com>
2021-11-30 19:02:39 +0800

    feat:初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/auth/Login";
 
 const routes = [
   {
@@ -16,6 +17,11 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  }
 ];
 
 const router = createRouter({
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <label>
+      <input value="账号" />
+      <input value="密码" type="password" />
+      <input type="button" @click="login()" value="登录" />
+    </label>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+axios.defaults.headers.post["Content-Type"] =
+  "application/x-www-form-urlencoded";
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    login() {
+      axios.get("").then((res) => {
+        alert("请求成功，response=" + res.data);
+        // data = res.data;
+        console.log(res.data.navList);
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
